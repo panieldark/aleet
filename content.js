0928083a93bc7b0b76fc8af45ab5e4d3d1133658
danielpark <danielpark1140@gmail.com>
2020-08-07 21:27:32 -0400

    Record info per site visited and display on extension use

--- content.js
+++ content.js
@@ -4,3 +4,9 @@
 // 	const matches = document.documentElement.innerHTML.match(re)
 // 	sendResponse({count: matches.length})
 // })
+const re = new RegExp('test','gi')
+const matches = document.documentElement.innerHTML.match(re)
+chrome.runtime.sendMessage({
+	url: window.location.href,
+	count: matches.length
+})
--- popup.js
+++ popup.js
@@ -1,16 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-	document.querySelector('button').addEventListener('click', onclick, false)
-	
-	function onclick() {
-		chrome.tabs.query({currentWindow: true, active: true}, 
-		function(tabs){
-			chrome.tabs.sendMessage(tabs[0].id,'hi', setCount)
-		})
-	}
-	function setCount(res) {
-		const div =document.createElement('div')
-		div.textContent = `${res.count} test`
+
+	const bg = chrome.extension.getBackgroundPage()
+	Object.keys(bg.tests).forEach(function(url) {
+		const div = document.createElement('div')
+		div.textContent = `${url}: ${bg.tests[url]}`
 		document.body.appendChild(div)
-	}
+	})
 
 }, false)
