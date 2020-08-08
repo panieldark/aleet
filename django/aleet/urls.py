00a71fee355d3dfeb795bd569f1cbb9e143fb259
danielpark <danielpark1140@gmail.com>
2020-08-07 22:54:20 -0400

    Working csrf_token for POST requests

--- django/aleet/urls.py
+++ django/aleet/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ping, name="ping")
+    path('', ping, name="ping"),
+    path('get_token', give_token, name="give_token")
 ]
--- django/aleet/views.py
+++ django/aleet/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.middleware.csrf import get_token
 
-@csrf_exempt
 def ping(request):
 	if request.method == "POST":
 		data = {}
 		data['message'] = 'success'
 		return JsonResponse(data)
+
+def give_token(request):
+	csrf_token = get_token(request)
+	return JsonResponse({'csrf': csrf_token})
