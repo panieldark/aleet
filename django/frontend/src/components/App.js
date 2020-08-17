4a3326c50bfd7177254d112b43387683e52aba11
danielpark <danielpark1140@gmail.com>
2020-08-17 17:44:50 -0400

    Build React front-end reflecting Django leads/messages

--- django/frontend/src/components/App.js
+++ django/frontend/src/components/App.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
 import store from "../store";
+import Leads from "./leads/Leads";
+import leads from "../reducers/leads";
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <h1>React App</h1>;
-      </Provider>
-    );
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<h1>React App</h1>;
+				<Leads />
+			</Provider>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
